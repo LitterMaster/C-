a604d9df989a2e68387cf94b1d8c992163783e6a
LitterMaster <lvyanfree@163.com>
2018-01-03 17:28:16 +0800

    Update 冒泡排序法.cpp

--- 冒泡排序法.cpp
+++ 冒泡排序法.cpp
@@ -3,3 +3,39 @@
 //
 //
 #include <stdio.h>
+
+int main( )
+{
+    
+    int i,j;
+    int a[10];
+    int temp;
+
+    printf("请输入数组：");
+
+    for(int i = 0; i < 10; i++)
+    {
+        scanf("%d\n",&a[i]);
+    }
+
+    for(int i = 0; i < 10; i++)
+    {
+        for(int j = 0; j < 9-i; j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }         
+        }   
+    }
+    
+    for(int i = 0; i < 10; i++)
+    {
+        printf("%d\t",a[i]);
+    }
+    
+    return 0;
+}
+
