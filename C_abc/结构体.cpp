72b854b3ad1dfcda4d4c588306dcf653da3d794d
LitterMaster <lvyanfree@163.com>
2018-01-12 15:23:02 +0800

    Update 结构体.cpp

--- C_abc/结构体.cpp
+++ C_abc/结构体.cpp
@@ -29,6 +29,13 @@ int main(int argc, char const *argv[])
     printf("height=%dcm\n",sana.height);
     printf("weight=%.2fkg\n",sana.weight);
     printf("schols=%ld$\n",sana.schols);
+    
+    //初始化
+    struct gs tala ={"Tala",165,48.2,20000};
+    printf("name  =%s\n",tala.name);
+    printf("height=%dcm\n",tala.height);
+    printf("weight=%.2fkg\n",tala.weight);
+    printf("schols=%ld$\n",tala.schols);
 
     return 0;
 }
